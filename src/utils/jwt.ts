ba3abef891eb8bb1baebb19ca3cdfae718b6cf95
singhdipesh14 <58405253+singhdipesh14@users.noreply.github.com>
2022-03-20 19:18:40 +0530

    .

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -29,7 +29,7 @@ export const attachCookiesToResponse = ({ res, user }: cookiesPayload) => {
 		expires: new Date(Date.now() + oneDay),
 		secure: process.env.NODE_ENV === "production",
 		signed: true,
-		domain: process.env.COOKIE_DOMAIN,
-		sameSite: "none"
+		// domain: process.env.COOKIE_DOMAIN,
+		// sameSite: "none"
 	})
 }
